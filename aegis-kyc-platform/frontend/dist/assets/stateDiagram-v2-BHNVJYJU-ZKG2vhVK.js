79fe67b667246d30c6dc9a78fdc323c4f0b84352
RIsHaBHMiShrA2710 <rishabhm2710@example.com>
2026-06-16 23:58:42 +0530

    feat: track built frontend files to allow deployment on JupyterHub pod without npm install

---
+++ aegis-kyc-platform/frontend/dist/assets/stateDiagram-v2-BHNVJYJU-ZKG2vhVK.js
@@ -0,0 +1,1 @@
+import{s as t,b as r,a,S as s}from"./chunk-AQP2D5EJ-3XXBpyFd.js";import{_ as i}from"./index-BIkZmGFv.js";import"./chunk-55IACEB6-D0I_roGL.js";import"./chunk-2J33WTMH-_dN5MSbd.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
